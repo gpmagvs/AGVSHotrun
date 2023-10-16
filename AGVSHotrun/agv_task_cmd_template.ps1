9db01079671297727fc8229fe6a3d3ccd88df006
jinweihuang <jinweihuang2033@outlook.com>
2023-10-16 16:46:34 +0800

    任務ps template 新增CSTType=Tray/修正地圖讀取流程

--- AGVSHotrun/agv_task_cmd_template.ps1
+++ AGVSHotrun/agv_task_cmd_template.ps1
@@ -2,7 +2,7 @@ $session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
 $session.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
 $session.Cookies.Add((New-Object System.Net.Cookie("connect.sid", "this_is_connect.sid", "/", "127.0.0.1")))
 $session.Cookies.Add((New-Object System.Net.Cookie("io", "this_is_io", "/", "127.0.0.1")))
-Invoke-WebRequest -UseBasicParsing -Uri "http://127.0.0.1:6600/mission/request?CarName=AGV_1&AGVID=1&Action=Move&FromStation=35&FromSlot=1&ToStation=&ToSlot=1&Priority=5&RepeatTime=1&CSTID=" `
+Invoke-WebRequest -UseBasicParsing -Uri "http://127.0.0.1:6600/mission/request?CarName=AGV_1&CSTType=Tray&AGVID=1&Action=Move&FromStation=35&FromSlot=1&ToStation=&ToSlot=1&Priority=5&RepeatTime=1&CSTID=" `
 -WebSession $session `
 -Headers @{
 "Accept"="text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
